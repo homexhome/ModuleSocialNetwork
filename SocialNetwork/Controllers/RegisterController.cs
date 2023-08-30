7782b8d3384dce8ac7c22936251ce0d31b4e4aba
homexhome <homexhome@proton.me>
2023-08-30 22:20:10 +0300

    Started working of IUnitOfWork

---
+++ SocialNetwork/Controllers/RegisterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.Web.Controllers
+{
+    public class RegisterController
+    {
+        // TODO after adding views
+        //[Route("Register")]
+        //[HttpGet]
+        //public IActionResult Register() {
+        //    return View("Home/Register");
+        //}
+
+        //[Route("RegisterPart2")]
+        //[HttpGet]
+        //public IActionResult RegisterPart2(RegisterViewModel model) {
+        //    return View("RegisterPart2", model);
+        //}
+    }
+}
