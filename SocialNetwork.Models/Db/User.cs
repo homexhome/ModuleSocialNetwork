50888b06b80b23794804d4cb0cf577db2b012f23
Дмитрий Анисимов <holder9191@gmail.com>
2023-09-01 15:39:56 +0300

    Migration done

--- SocialNetwork.Models/Db/User.cs
+++ SocialNetwork.Models/Db/User.cs
@@ -8,16 +8,16 @@ namespace SocialNetwork.Models.Db
         public string LastName { get; set; }
         public string MiddleName { get; set; } = string.Empty;
         public DateTime BirthDate { get; set; }
-        //public string Image { get; set; }
-        //public string Status { get; set; }
-        //public string About { get; set; }
+        public string Image { get; set; }
+        public string Status { get; set; }
+        public string About { get; set; }
 
-        //public string GetFullName() { return String.Join(' ', FirstName, LastName); }
+        public string GetFullName() { return String.Join(' ', FirstName, LastName); }
 
-        //public User() {
-        //    Image = "https://via.placeholder.com/500";
-        //    Status = "OK";
-        //    About = "About me";
-        //}
+        public User() {
+            Image = "https://via.placeholder.com/500";
+            Status = "OK";
+            About = "About me";
+        }
     }
 }
