460908ed32e16dd23c142ad6099544cebb82645a
Дмитрий Анисимов <holder9191@gmail.com>
2023-09-01 15:51:52 +0300

    UserViewModel start

---
+++ SocialNetwork.Models/ViewModels/Account/UserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using SocialNetwork.Models.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Models.ViewModels.Account
+{
+    public class UserViewModel
+    {
+        public User User { get; set; }
+
+        public UserViewModel(User user)
+        {
+            User = user;
+        }
+
+    }
+}
