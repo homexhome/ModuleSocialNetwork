fe99afab556b2d5e0a9d5919eb272da965a0cac7
homexhome <homexhome@proton.me>
2023-09-05 20:10:01 +0300

    Uncommit Database.Migrate() in ApplicationDbContext

--- SocialNetwork.Data/Context/ApplicationDbContext.cs
+++ SocialNetwork.Data/Context/ApplicationDbContext.cs
@@ -9,7 +9,7 @@ namespace SocialNetwork.Data.Context
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            //Database.Migrate();
+            Database.Migrate();
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
